6de305829065bc710d1d2274567c5cce56c2b00a
Tejas Parikh <121845727+tejas-algo@users.noreply.github.com>
2023-12-27 23:56:22 +0530

    footer fixes, and synesis icon

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,18 @@
+import React from 'react'
 import './Footer.css'
 import logo from '../../assets/logo-nobg.png'
 
 export default function Footer() {
   return (
     <footer className="footer">
-      <img src={logo} alt="Company Logo" className="footer-logo" />
+      <img
+        src={logo}
+        alt="Company Logo"
+        className="footer-logo"
+        onClick={() => {
+          window.open('https://metawork.securetool.company', '_blank')
+        }}
+      />
     </footer>
   )
 }
